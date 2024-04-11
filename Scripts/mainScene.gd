4810a8ca0c1fedb9ebee5392964aa662081a5fe5
David Rose <david.rose.jack@gmail.com>
2024-04-11 13:06:47 -0400

    Added quit function, mapped to ESC

--- Scripts/mainScene.gd
+++ Scripts/mainScene.gd
@@ -38,7 +38,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta):
+func _process(_delta):
+	if (Input.is_action_pressed("get_outta_here")):
+		get_tree().quit()
 #	pass
 
 
